3f321be3e65d33a12ba2d8bdb10d384ca4e7c7b5
akiko0311 <119967183+chanhtima@users.noreply.github.com>
2023-09-23 13:24:04 +0700

    update tmp

---
+++ src/components/Carousel/NewCarousel.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import CardDateBasuc01 from "../Card/CardDateBasuc01";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { DataNew } from "../../data/DataNew"
+ 
+function NewCarousel() {
+  const dataCardSlide = DataNew.map((item) => {
+    return (
+      <SplideSlide key={item.id} className="p-3">
+        <CardDateBasuc01
+          image={item.image}
+          title={item.title}
+          detail={item.detail}
+           url={item. url}
+          date={item.date}
+
+          Btn={item.Btn}
+        />
+      </SplideSlide>
+    );
+  });
+  return (
+    <div>
+      <Splide
+        aria-label="NewEvents"
+        options={{
+          perPage: 3,
+          pagination: false,
+          breakpoints: {
+            992: {
+              perPage: 2,
+            },
+            768: {
+              perPage: 1,
+            },
+          },
+        }}
+      >
+        {dataCardSlide}
+      </Splide>
+    </div>
+  );
+}
+
+export default NewCarousel;
